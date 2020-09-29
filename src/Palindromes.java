8459f5e21bc43c210f1cf6cc71b3436979c77f17
Drew <andrewburns.r@gmail.com>
2020-09-29 17:25:41 -0600

    added printBox method

--- src/Palindromes.java
+++ src/Palindromes.java
@@ -19,7 +19,14 @@ public class Palindromes {
             }
             boolean result = Tools.isPalindrome(userInput);
             if (result == true) {
-                System.out.println("That's a palindrome! Nice!\n");
+                System.out.println("That's a palindrome! Nice!\nWould you like to print it in box form? (y/n)");
+                // user is asked whether they want to print it in box form or not;
+                // a placeholder called "palindrome" is made so that the userInput can change
+                String palindrome = userInput;
+                userInput = scanner.nextLine();
+                if (userInput.toLowerCase().equals("y")){
+                    Tools.printBox(palindrome);
+                }
             }
             else {
                 System.out.println("Sorry, that's not a palindrome!\n");
--- src/Tools.java
+++ src/Tools.java
@@ -1,18 +1,38 @@
 public class Tools {
 
-    public static boolean isPalindrome(String str){
-        // setting up two pointers to creep towards the middle of a char array
+    public static boolean isPalindrome(String str) {
+        // initializing two pointers to creep towards the middle of a char array
         int left = 0;
         int right = str.length() - 1;
         char[] array = str.toCharArray();
 
         while (left < right) {
             if (array[left] != array[right]) {
-                 return false;
+                return false;
             }
             left++;
             right--;
         }
         return true;
+    }
+
+    public static void printBox(String str) {
+
+        // amount of space between each side of the box, then making a string w/ that amount
+        int spacesInt = str.length() - 2;
+        String spaces = "";
+        for (int i = 0; i < spacesInt; i++) {
+            spaces += " ";
+        }
+
+        // top row
+        System.out.println("\n" + str);
+        // middle rows
+        for (int i = 0; i < spacesInt; i++) {
+            System.out.println(str.charAt(i + 1) + spaces + str.charAt(i + 1));
+        }
+        // last row
+        System.out.println(str + "\n");
+
     }
 }
